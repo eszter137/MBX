c529a9da5b443a7c4fac4fa0159387c985bd10c3
eszter137 <szeszter94@gmail.com>
2020-07-22 18:31:53 +0100

    testing non-orthogonal boxes - supercell of nonorthogonal box

---
+++ my_tests/test_nonorthogonal_cell/order_molecules.py
@@ -0,0 +1,16 @@
+import quippy
+import quippy
+import ase
+import numpy as np
+import os
+import sys
+from collections import OrderedDict
+sys.path.insert(0,"/home/es732/programs/function_modules/")
+import unwrap_oh2_ch4
+
+
+this_file_name=sys.argv[1]
+new_file_name=this_file_name.split(".xyz")[0]+"_ordered.xyz"
+atoms_in=quippy.Atoms(this_file_name)
+atoms=unwrap_oh2_ch4.order_molecules(atoms_in)["ordered_mw"]
+ase.io.write(new_file_name,atoms) 
