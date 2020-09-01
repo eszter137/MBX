9e87de31071593cfe47464effdbb88592dcf4c23
Eszter Szekely <szeszter94@gmail.com>
2020-09-01 14:01:16 +0100

    Added filled ice c1te to nonorthogonal energy test calculations.

---
+++ my_tests/test_nonorthogonal_cell/c1te/build_supercell_nonsym.py
@@ -0,0 +1,21 @@
+import quippy
+from ase.calculators.calculator import all_changes
+import quippy
+import ase
+import numpy as np
+import os
+import sys
+from collections import OrderedDict
+sys.path.insert(0,"/home/es732/Eszter/function_modules/")
+import unwrap_oh2_ch4
+
+
+this_file_name=sys.argv[1]
+s_ind_list=[2,3,4]
+atoms = ase.io.read(this_file_name)
+atoms_unwrapped = unwrap_oh2_ch4.unwrap_water_methane(atoms)
+atoms=ase.Atoms(atoms_unwrapped["phase_mw"] )
+atoms = atoms.repeat(s_ind_list)
+new_file_name=this_file_name.split(".xyz")[0]+"_"+str(s_ind_list[0])+"x"+str(s_ind_list[1])+"x"+str(s_ind_list[2])+".xyz"
+
+ase.io.write(new_file_name,atoms) 
